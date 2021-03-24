b94311e9fb3620301dd37886e03d06299aab31b2
Cassidy Duray <cduray@css.edu>
2021-03-24 11:26:51 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/css/currency_converter/MainActivity.kt
@@ -0,0 +1,6 @@
+package css.currency_converter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CIS 3334 Currency Converter',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+
+final dollarTextField = TextEditingController();
+final euroTextField = TextEditingController();
+double _dollars = 0;
+double _euros = 0;
+double _conversionRate = 1.19;
+
+convertToDollars(){
+  _euros = double.parse(euroTextField.text);
+  _dollars = _euros * _conversionRate;
+  dollarTextField.text = _dollars.toStringAsFixed(2);
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Dollars:',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            TextField(
+              controller: dollarTextField,
+            ),
+            ElevatedButton(
+                onPressed: (){
+                  _dollars = double.parse(dollarTextField.text);
+                  _euros = _dollars * _conversionRate;
+                  euroTextField.text = _euros.toString();
+                },
+                child: Text('Convert to Euros')),
+            Text(
+              'Euros:',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            TextField(
+              controller: euroTextField,
+            ),
+            ElevatedButton(
+              child: Text('Convert to Dollars'),
+              onPressed: convertToDollars,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
